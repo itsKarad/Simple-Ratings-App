23d60990ad6a1fdc3d977331a7bfa916a1ddcaf0
Karad <akarad@ee.iitr.ac.in>
2021-12-28 13:02:45 +0530

    Added middleware so CORS won't trouble me

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const app = express();
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+// CORS workaround
+app.use((req, res, next) => {
+    // Adding headers before forwarding
+    // Which domains should be allowed?
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    // Which methods should be allowed?
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, DELETE, OPTIONS",
+        "Access-Control-Allow-Origin",
+    );
+    // Which headers should be allowed?
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+    );
+
+    next();
+});
+
 app.get("/", (req, res) => {
     res.send("Working.");
 });
+
 
 
 const PORT = 8000;
