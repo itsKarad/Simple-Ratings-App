6e4fb49f37e32cb49d7e29778604f7993c757757
Karad <akarad@ee.iitr.ac.in>
2021-12-28 10:52:52 +0530

    Added basic layout of app, divided it into sections

---
+++ src/components/ProductReviewSection.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './ProductReviewSection.css';
+
+const ProductReviewSection = (props) => {
+    return (
+        <div className="product-item">
+            <div class = "product-info">
+                <h1 className = "product-title">The Minimalist Entrepreneur</h1>
+            </div>
+            <div className = "product-ratings">
+                <div className = "product-rating">
+                    {1.2}
+                </div>            
+                <div className = "product-rating-form">
+                    <div className = "product-actions">
+                        <button class = "btn" onClick = {() => {}}>Add review</button>
+                    </div>
+                </div>
+                
+            </div>
+            <div className = "divider">
+
+            </div>
+            <div class = "product-reviews">
+                <h2 class = "reviews-header">Reviews</h2>
+            </div>
+        </div>
+    )
+};
+
+export default ProductReviewSection;
